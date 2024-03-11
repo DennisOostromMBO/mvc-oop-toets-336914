22e261a3dcfe4a4143116ec8a540c89780fdf2ba
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-03-11 13:07:51 +0100

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     * Hieronder staan de database-connectie constanten
+     * die worden gebruikt in de database class
+     */
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvcframework-2309b');
+
+
+    /**
+     * Dit is het pad naar de app directory
+     */
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    /**
+     * Dit is de naam van de virtual hostnaam
+     */
+    define('URLROOT', 'http://www.mvc-framework-2309b.org');
---
+++ app/views/homepage/index.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+
+    <?php
+        echo $data['title'];
+    ?>
+
+    <a href='<?= URLROOT; ?>/Instructeur/Index'>Instructeur pagina</a> |
+    <a href='<?= URLROOT; ?>/Cars/Index'>De auto pagina</a> | 
+    <a href='<?= URLROOT; ?>/Countries/index'>Landen</a>
+    
+<?php require APPROOT . '/views/includes/footer.php';
+    
+
